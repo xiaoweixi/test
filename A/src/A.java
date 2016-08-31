196dc320cf3cc661e23b48aefa637c78fecb0d33
XiaoWeiXi <XiaoWeiXi@xiaoweixi-PC>
2016-08-31 19:41:13 +0800

    爱爱爱

---
+++ A/src/A.java
@@ -0,0 +1,25 @@
+/**  
+ * @Title: A.java
+ * @Package 
+ * @Description: TODO(用一句话描述该文件做什么)
+ * @author XiaoWeiXi  
+ * @date  2016年8月31日
+ * @version V1.0  
+ */
+
+/**    
+ * 项目名称：乐播管理后台 
+ * 类描述：
+ * 类名称：.A     
+ * 创建人： XiaoWeiXi 
+ * 创建时间：2016年8月31日 下午7:40:57   
+ * 修改人：
+ * 修改时间：2016年8月31日 下午7:40:57   
+ * 修改备注：   
+ * 版权所有：深圳乐播科技 2016
+ * @version   V1.0    
+ */
+
+public class A {
+
+}
